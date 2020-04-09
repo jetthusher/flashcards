0f35e3da99b2b1b4d6e5c37371945fa890f7eb0b
Jett Husher <jetthusher@pm.me>
2020-04-09 09:58:20 +0000

    refactor(saga): rename services getter from context

--- src/features/counter/sagas/index.ts
+++ src/features/counter/sagas/index.ts
@@ -2,33 +2,33 @@ import { put, select } from "redux-saga/effects"
 import { SagaIterator } from "redux-saga"
 import { createSetAction } from "../actions"
 import { Services } from "../../../services/types"
-import getServicesFromContext from "../../../store/getServicesFromContext"
+import getServicesFromSagaContext from "../../../store/getServicesFromSagaContext"
 import { getCounter } from "../selectors"
 
 export function* syncStateSaga(): SagaIterator {
   const state: number = yield select(getCounter)
-  const services: Services = yield getServicesFromContext()
+  const services: Services = yield getServicesFromSagaContext()
   const value = services.counter.setValue(state)
   const setAction = createSetAction(value)
   yield put(setAction)
 }
 
 export function* incrementSaga(): SagaIterator {
-  const services: Services = yield getServicesFromContext()
+  const services: Services = yield getServicesFromSagaContext()
   const value = services.counter.increment()
   const setAction = createSetAction(value)
   yield put(setAction)
 }
 
 export function* decrementSaga(): SagaIterator {
-  const services: Services = yield getServicesFromContext()
+  const services: Services = yield getServicesFromSagaContext()
   const value = services.counter.decrement()
   const setAction = createSetAction(value)
   yield put(setAction)
 }
 
 export function* resetSaga(): SagaIterator {
-  const services: Services = yield getServicesFromContext()
+  const services: Services = yield getServicesFromSagaContext()
   const value = services.counter.reset()
   const setAction = createSetAction(value)
   yield put(setAction)
